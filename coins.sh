06f0ee127b6b42b00683d04a4625f7d928eb114a
Ashok Mane <ashokmane81800@gmail.com>
2021-03-08 16:31:31 +0530

    [ashok] created coins.sh

---
+++ coins.sh
@@ -0,0 +1,16 @@
+#! /bin/bash -x
+
+read -p "enter the number between 1 to 4 :"number
+
+case $number in
+1 ) echo "entered number one"
+;;
+2 ) echo"entered number is two"
+;;
+3 ) echo "entered is three"
+;;
+4 ) echo"entered is four"
+;;
+* ) echo "you did not enter between 1 to 4"
+
+esacs
